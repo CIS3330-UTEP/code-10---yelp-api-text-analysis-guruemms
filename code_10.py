a0e88101bf1a2aa91ece0848feceec3a954d9e61
guruemms <123012403+guruemms@users.noreply.github.com>
2023-04-17 11:10:56 -0600

    Final

--- code_10.py
+++ code_10.py
@@ -18,6 +18,11 @@ search_limit = 20
 search_results = yelp_api.search_query(term=search_term,location = search_location, sort_by = search_sort_by, limit = search_limit)
 #use offset = ___ to move further down list of resturants
 
+for business in search_results['businesses']:
+    print(business['name'])
+    print(business['alias'])
+    print("\n")
+
 id_for_reviews = ("proof-and-press-el-paso")
 reviews_result = yelp_api.reviews_query(id = id_for_reviews)
 reviews_df = pd.DataFrame.from_dict(reviews_result['reviews'])
@@ -28,6 +33,33 @@ for review in reviews_df['text']:
     print('\n')
 
 id_for_reviews = ("sand-dust-coffee-el-paso")
+reviews_result = yelp_api.reviews_query(id = id_for_reviews)
+reviews_df = pd.DataFrame.from_dict(reviews_result['reviews'])
+for review in reviews_df['text']:
+    sentiment_score = analyzer.polarity_scores(review)
+    print(review)
+    print(sentiment_score)
+    print('\n')
+
+id_for_reviews = ("vyable-coffee-el-paso")
+reviews_result = yelp_api.reviews_query(id = id_for_reviews)
+reviews_df = pd.DataFrame.from_dict(reviews_result['reviews'])
+for review in reviews_df['text']:
+    sentiment_score = analyzer.polarity_scores(review)
+    print(review)
+    print(sentiment_score)
+    print('\n')
+
+id_for_reviews = ("global-coffee-el-paso")
+reviews_result = yelp_api.reviews_query(id = id_for_reviews)
+reviews_df = pd.DataFrame.from_dict(reviews_result['reviews'])
+for review in reviews_df['text']:
+    sentiment_score = analyzer.polarity_scores(review)
+    print(review)
+    print(sentiment_score)
+    print('\n')
+
+id_for_reviews = ("perks-coffee-el-paso")
 reviews_result = yelp_api.reviews_query(id = id_for_reviews)
 reviews_df = pd.DataFrame.from_dict(reviews_result['reviews'])
 for review in reviews_df['text']:
